27acc87ac13495f749597c72fdc17be105875bc2
Alexey Korkota <alexey.korkota@gmail.com>
2024-09-11 08:57:38 +0300

    Add the git prompt install

--- .bin/install.sh
+++ .bin/install.sh
@@ -13,6 +13,7 @@ if [ $? = 0 ]; then
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
+curl -o $HOME/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 config checkout
 config config status.showUntrackedFiles no
 
