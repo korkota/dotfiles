2b607a491a7fc7a17276f3936af105a44880b83a
Alexey Korkota <alexey.korkota@gmail.com>
2024-09-12 05:04:03 +0300

    Add tmux and lazygit to deps

--- .bin/install_deps.sh
+++ .bin/install_deps.sh
@@ -23,4 +23,14 @@ sudo apt install -y fd-find
 echo "Installing xclip..."
 sudo apt install -y xclip
 
+echo "Installing tmux..."
+sudo apt install -y tmux
+
+echo "Installing lazygit..."
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm -rf lazygit
+
 echo "Deps have been installed."
