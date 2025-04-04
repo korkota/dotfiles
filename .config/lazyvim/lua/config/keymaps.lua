d280cdd68c8cbf8211081e0af31c82e4b6c45c9b
Alexey Korkota <alexey.korkota@gmail.com>
2025-04-04 22:44:50 +0300

    Update the LazyVim config

--- .config/lazyvim/lua/config/keymaps.lua
+++ .config/lazyvim/lua/config/keymaps.lua
@@ -2,13 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.del("t", "<c-h>")
-vim.keymap.del("t", "<c-j>")
-vim.keymap.del("t", "<c-k>")
-vim.keymap.del("t", "<c-l>")
-
 -- vim.keymap.set("n", "<C-d>", "<C-d>zz")
 -- vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "]<Space>", "m`o<Esc>``")
+vim.keymap.set("n", "[<Space>", "m`O<Esc>``")
 
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
