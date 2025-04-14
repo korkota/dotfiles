49481eeb819c6a00b587fb211509da6b2b69cfc4
Alexey Korkota <alexey.korkota@gmail.com>
2025-04-14 08:01:30 +0300

    Remove the .vimrc

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,16 +3,12 @@
 -- Add any additional options here
 
 vim.g.lazyvim_prettier_needs_config = false
+vim.g.snacks_animate = false
 vim.g.undotree_WindowLayout = 3
 
-vim.opt.spelllang = { "ru", "en_us" }
 vim.opt.breakindent = true
-
-vim.opt.undofile = true
-vim.opt.swapfile = false
-
 vim.opt.clipboard = ""
-
--- vim.opt.colorcolumn = "80"
-
 vim.opt.shell = "/bin/bash"
+vim.opt.spelllang = { "ru", "en_us" }
+vim.opt.swapfile = false
+vim.opt.undofile = true
