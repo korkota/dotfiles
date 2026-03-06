c90dba13fc54b9593b8e7fe5039ba3c013a4ece8
Alexey Korkota <alexey.korkota@gmail.com>
2026-03-06 03:54:12 +0300

    Update dotfiles

--- .bin/myarchinstall.sh
+++ .bin/myarchinstall.sh
@@ -5,8 +5,8 @@ set -e
 echo "Installing deps..."
 
 sudo pacman -Syuq --noconfirm alacritty base-devel bash-completion chromium curl fd fzf git gnome gnome-tweaks keyd lazydocker lazygit \
-less lua-jsregexp man man-db neovim networkmanager nvm obsidian openssh ripgrep sudo syncthing tldr \
-tmux trash-cli tree-sitter-cli ttf-jetbrains-mono-nerd virtualbox-guest-utils vlc xclip zip
+  less lua-jsregexp man man-db neovim networkmanager nvm obsidian openssh ripgrep sudo syncthing tldr \
+  tmux trash-cli tree-sitter-cli ttf-jetbrains-mono-nerd virtualbox-guest-utils vlc xclip zip
 
 curl -Lks https://raw.githubusercontent.com/korkota/dotfiles/main/.bin/install.sh | /bin/bash
 source "$HOME/.profile"
@@ -27,7 +27,7 @@ sudo usermod -aG keyd "$(whoami)"
 mkdir -p ~/.local/share/gnome-shell/extensions
 ln -s /usr/share/keyd/gnome-extension-45 ~/.local/share/gnome-shell/extensions/keyd\@keyd.rvaiya.github.com
 
-echo "Installing complition for tmux..."
+echo "Installing compilation for tmux..."
 dir="${BASH_COMPLETION_DIR:-"${XDG_DATA_HOME:-"$HOME/.local/share"}/bash-completion"}/completions"
 mkdir -p "$dir"
 curl -fSsL "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux" >"${dir?error: dir not set: you must run the previous commands first}/tmux"
@@ -50,12 +50,13 @@ gnome-extensions install --force gnome-bedtime-mode_23.0.zip
 rm gnome-bedtime-mode_23.0.zip
 
 echo "Installing node..."
-. "/usr/share/nvm/init-nvm.sh"
+. /usr/share/nvm/init-nvm.sh
 nvm install node
 
-echo "Adding the layout switch hotkey..."
-dconf write /org/gnome/desktop/wm/keybindings/switch-input-source-backward "['<Shift>Alt_L']"
-dconf write /org/gnome/desktop/wm/keybindings/switch-input-source "['<Alt>Shift_L']"
+echo "Adding the layout switch shortkey..."
+sudo localectl set-x11-keymap us,ru "" "" grp:alt_shift_toggle
+# dconf write /org/gnome/desktop/wm/keybindings/switch-input-source-backward "['<Shift>Alt_L']"
+# dconf write /org/gnome/desktop/wm/keybindings/switch-input-source "['<Alt>Shift_L']"
 
 echo "Enabling gdm..."
 sudo systemctl enable gdm.service
@@ -67,7 +68,7 @@ makepkg -si --noconfirm
 paru --gendb
 
 echo "Installing g3kb-switch..."
-paru --noconfirm --useask -S g3kb-switch 
+paru --noconfirm --useask -S g3kb-switch
 
 echo "Installing lscolors..."
 paru --noconfirm --useask -S lscolors
